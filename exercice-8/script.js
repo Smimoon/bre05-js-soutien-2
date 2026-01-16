6a893c1cc956c64d84a54f7c7dad0e6ae10a5fb2
smimoon <simonlaroche@orange.fr>
2026-01-16 14:51:55 +0000

    Save end exercice-8

--- exercice-8/script.js
+++ exercice-8/script.js
@@ -1,3 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
+    let li = document.querySelectorAll("li");
     
+    for(let i=0; i<li.length; i++) {
+        if(i% 2 !== 0) {
+            li[i].style.color="blue";
+        }
+    }
 })

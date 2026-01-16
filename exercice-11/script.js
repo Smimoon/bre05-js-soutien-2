6270a56d4aee2dc75626ffa76d1a6c6a1dc08d45
smimoon <simonlaroche@orange.fr>
2026-01-16 15:56:00 +0000

    Save end exercice-11

--- exercice-11/script.js
+++ exercice-11/script.js
@@ -1,3 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
+    let carre = document.getElementById("carre");
     
+    carre.addEventListener("click", (event) => {
+        carre.style.left = "50px";
+    })
 })
